fe86e2eeb4bba1b2953a1fa651e9df47963611b4
Hannes Hapke <hannes@talentpair.com>
2018-03-18 10:56:08 -0700

    first commit

---
+++ create_pairs.py
@@ -0,0 +1,51 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+def get_text(url):
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'lxml')
+
+    text = soup.get_text()
+
+    # # clean up
+    # regex_remove_names = re.compile(r"[A-Z]{2,}")
+    # text = regex_remove_names.sub("", text)
+
+    regex_redundant_linebreaks = re.compile(r"\n{2,}")
+    text = regex_redundant_linebreaks.sub("\n", text)
+    ltext = text.split("\n")
+    return ltext
+
+def get_tags(url):
+    resp = requests.get(url)
+    text = resp.text
+    split_text = text.split("\n")
+
+    ltext = []
+    for l in split_text:
+        if l == '<T>':
+            lines = []
+        elif l == '</T>':
+            ltext.append(lines)
+        else:
+            lines.append(l)
+
+    return ltext
+
+def get_pairs(name):
+
+    org_url = "https://raw.githubusercontent.com/aforsyth/nfs-webscraper/355fba07d70d298597efe429fe3226d8360c49c4/output/{}_{}.txt".format(name, "original")
+
+    mdn_url = "https://raw.githubusercontent.com/aforsyth/nfs-webscraper/355fba07d70d298597efe429fe3226d8360c49c4/output/{}_{}.txt".format(name, "modern")
+
+    a = get_tags(org_url)
+    b = get_tags(mdn_url)
+
+    rv = list(zip(a, b))
+
+    return rv
+
+
+name = 'hamlet'
+l = get_pairs(name)
